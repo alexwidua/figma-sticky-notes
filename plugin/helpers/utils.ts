4a1fcd353242c0360346e96265d9a52fb13e7753
Alexander Widua <a.widua@gmail.com>
2020-11-07 13:51:27 +0100

    :fire: Remove randomCoord helper

--- plugin/helpers/utils.ts
+++ plugin/helpers/utils.ts
@@ -1,15 +1,10 @@
 import chroma from 'chroma-js';
 
-// Converts Hexcode to GL, which is a variant of RGBA
+// Converts Hex to GL, which is a variant of RGBA
 export function hexToGl(hex: string): RGB {
   return {
     r: chroma(hex).gl()[0],
     g: chroma(hex).gl()[1],
     b: chroma(hex).gl()[2]
   };
-}
-
-// Return random value
-export function randomCoord(spread: number): number {
-  return Math.floor(Math.random() * spread);
 }
