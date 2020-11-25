86cf44134f05cd8003f278f8da3f1151ee602fb3
Alexander Widua <a.widua@gmail.com>
2020-11-25 20:25:31 +0100

    :wrench: Remove webpack copy plugin

--- vue.config.js
+++ vue.config.js
@@ -1,45 +1,53 @@
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const Path = require('path');
 
 module.exports = {
   css: {
     extract: false
   },
   filenameHashing: false,
+  // Disable sourcemaps for Figma
+  productionSourceMap: false,
   chainWebpack: config => {
+    // Disable generation of default index.html document
     config.plugins.delete('html');
     config.plugins.delete('preload');
     config.plugins.delete('prefetch');
+    // Disable default code splitting
     config.optimization.delete('splitChunks');
+    // Disable default entry point since we are using our own
     config.entryPoints.delete('app');
+    // Encode svg assets as base64 for Figma since resources have to be embedded:
+    // https://www.figma.com/plugin-docs/resource-links/
+    // First, delete previous applied loaders
+    config.module.rule('svg').uses.clear();
+    config.module
+      .rule('svg')
+      .test(/\.svg$/)
+      .use('url-loader')
+      .loader('url-loader');
   },
   configureWebpack: {
     plugins: [
       new HtmlWebpackPlugin({
-        template: './ui/ui.html',
+        template: './ui/index.html',
         filename: 'ui.html',
         inlineSource: '.(js)$',
         chunks: ['ui']
       }),
-      new HtmlWebpackInlineSourcePlugin(),
-      new CopyPlugin([{ from: 'plugin/manifest.json', to: 'manifest.json' }])
+      new HtmlWebpackInlineSourcePlugin()
     ],
     entry: {
       ui: ['./ui/main.ts'],
       code: ['./plugin/index.ts']
-    },
-    resolve: {
-      alias: {
-        '@': Path.resolve('./ui')
-      }
     },
     output: {
       filename: '[name].js',
       chunkFilename: '[name].js'
     }
   },
-  productionSourceMap: false
-  // For Figma
+  // Serve devServer to ui.hmtl instead of /index.html
+  devServer: {
+    index: 'ui.html'
+  }
 };
